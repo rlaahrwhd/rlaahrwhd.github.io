8f358ec1c2d07c49659d69a573bbd01156250bcb
rlaahrwhd <rlaahrwhd@naver.com>
2024-07-03 17:06:40 +0900

    1

---
+++ _site/assets/js/theme.js
@@ -0,0 +1,13 @@
+const themeMap = {
+  'default': {
+    'background-color': '#222',
+    'text-color': 'white',
+    'highlight-color': '#2e2e2e'
+    
+  },
+  'white': {
+    'background-color': 'white',
+    'text-color': '#222',
+    'highlight-color': '#eee'
+  }
+}
